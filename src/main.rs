c9389fee6a4824fa8ce63a8392a29bbed621ef0a
Quinn <quinnwen.cn@gmail.com>
2025-03-08 22:50:14 +0800

    Add cli module
    Add cli module to parse Command Line.
    
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+
+use std::result::Result;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli_arg = cli::parse_cli()?;
+    println!("{:?}", cli_arg);
+
+    Ok(())
 }
