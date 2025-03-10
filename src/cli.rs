7b29fec13daeb9b252ddadd82d7c89da22472961
Quinn <quinnwen.cn@gmail.com>
2025-03-10 17:13:39 +0800

    Remove ECU
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -5,13 +5,12 @@ use std::error::Error;
 #[derive(Debug)]
 pub struct CliArg {
     pub seed: String,
-    pub key: String,
-    pub ecu: String,    
+    pub key: String,   
 }
 
 impl CliArg {
-    pub fn new(seed: String, key: String, ecu: String) -> Self {
-        Self { seed, key, ecu }
+    pub fn new(seed: String, key: String) -> Self {
+        Self { seed, key}
     }
 }
 
@@ -36,14 +35,6 @@ pub fn parse_cli() -> Result<CliArg, Box<dyn Error>> {
                 .help("Set the key value")
                 .value_parser(clap::value_parser!(String)),
         )
-        .arg(
-            Arg::new("ecu")
-                .short('e')
-                .long("ecu")
-                .value_name("ECU")
-                .help("Set the ECU value")
-                .value_parser(clap::value_parser!(String)),
-        )
         .get_matches();
 
     if !matches.args_present() {
@@ -51,14 +42,12 @@ pub fn parse_cli() -> Result<CliArg, Box<dyn Error>> {
         println!("Options:");
         println!("  -s, --seed <SEED>    Set the seed value");
         println!("  -k, --key <KEY>      Set the key value");
-        println!("  -e, --ecu <ECU>      Set the ECU name");
         println!("  -h, --help           Print help information");
         return Err("Invalid input".into());
     }
 
     let seed = matches.get_one::<String>("seed").ok_or("seed not found")?.to_owned();
     let key = matches.get_one::<String>("key").ok_or("key not found")?.to_owned();
-    let ecu = matches.get_one::<String>("ecu").ok_or("ecu not found")?.to_owned();
 
-    Ok(CliArg::new(seed, key, ecu))
+    Ok(CliArg::new(seed, key))
 }
--- src/process.rs
+++ src/process.rs
@@ -23,35 +23,9 @@ pub fn bytes_to_hex_string(bytes: &[u8]) -> String {
     hex::encode(bytes)
 }
 
-
-// high 8 bytes, low 8 bytes
-fn ecu_mask(ecu: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-    match ecu {
-        "ECU1" => Ok(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00]),
-        "ECU2" => Ok(vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]),
-        "ECU3" => Ok(vec![0xff, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee]),
-        _ => Err("Invalid ECU".into()),
-    }
-}
-
-fn xor_bytes(a: &Vec<u8>, b: &Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
-    if a.len() != b.len() {
-        let s: String = format!("Length mismatch, a: {}, b: {}", a.len(), b.len());
-        return Err(s.into());
-    }
-
-    Ok(a.iter()
-     .zip(b.iter())
-     .map(|(a, b)| a ^ b)
-     .collect())
-}
-
 pub fn process(arg: &cli::CliArg) -> Result<Vec<u8>, Box<dyn Error>> {
     let seed = hex_string_to_bytes(&arg.seed)?;
     let key = hex_string_to_bytes(&arg.key)?;
-    let mask = ecu_mask(&arg.ecu)?;
-
-    let mask_value = xor_bytes(&seed, &mask)?;
-    let cmac = crypto::calculate_cmac(&mask_value, &key)?;
+    let cmac = crypto::calculate_cmac(&key, &seed)?;
     Ok(cmac)
 }
